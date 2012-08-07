5acbdec76940bbb2c4f2d63996b0a9a030442f8b
hunt3ri <hunter_iain@yahoo.co.uk>
2012-08-07 21:58:17 +0100

    Update to deployment scripts to latest Powershell commandlets

--- DeployAzureApp.ps1
+++ DeployAzureApp.ps1
@@ -15,92 +15,58 @@ Param(  $serviceName = "",
 		$thumbprint = "",
 		$subscriptionId = "",
         
-        $Environment = "Staging",
-        $deploymentLabel = "ContinuousDeploy to $servicename",
+        $Environment = "",
+        $deploymentLabel = "",
         $timeStampFormat = "g",
         $alwaysDeleteExistingDeployments = 1,
-        $enableDeploymentUpgrade = 1,
+        $enableDeploymentUpgrade = 0,
         $selectedsubscription = "default",
-        $subscriptionDataFile = "c:\scripts\WindowsAzure\Subscription.xml"
+        $subscriptionDataFile = ""
      )
       
 
 #initialize cmdlet snapin and subscription
-if ((Get-PSSnapin | ?{$_.Name -eq "WAPPSCmdlets"}) -eq $null)
-{
-  Add-PSSnapin WAPPSCmdlets
-}
+Import-Module 'C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1'
+Set-ExecutionPolicy RemoteSigned
+
 
 $cert = Get-Item cert:\CurrentUser\My\$thumbprint 
 $subid = $subscriptionId 
 
 #Time to implant your code :-)
-set-subscription -subscriptionname inception -certificate $cert -subscriptionid $subid 
-select-subscription iain 
+set-azuresubscription -subscriptionname inception -certificate $cert -subscriptionid $subid -CurrentStorageAccount $storageAccountName
+select-azuresubscription inception 
 
-$subscription = Get-Subscription inception
+$subscription = Get-AzureSubscription inception
 $subscriptionname = $subscription.subscriptionname
 $subscriptionid = $subscription.subscriptionid
 $slot = $environment
 
 
-function SuspendDeployment()
-{
-	write-progress -id 1 -activity "Suspending Deployment" -status "In progress"
-	Write-Output "$(Get-Date –f $timeStampFormat) - Suspending Deployment: In progress"
 
-	$suspend = Set-DeploymentStatus -Slot $slot -ServiceName $serviceName -Status Suspended
-	$opstat = Get-OperationStatus -operationid $suspend.operationId
-	
-	while ([string]::Equals($opstat, "InProgress"))
-	{
-		sleep -Seconds 1
-
-		$opstat = Get-OperationStatus -operationid $suspend.operationId
-	}
-
-	write-progress -id 1 -activity "Suspending Deployment" -status $opstat
-	Write-Output "$(Get-Date –f $timeStampFormat) - Suspending Deployment: $opstat"
-}
 
 function DeleteDeployment()
 {
-	SuspendDeployment
-
 	write-progress -id 2 -activity "Deleting Deployment" -Status "In progress"
 	Write-Output "$(Get-Date –f $timeStampFormat) - Deleting Deployment: In progress"
 
-	$removeDeployment = Remove-Deployment -Slot $slot -ServiceName $serviceName
-	$opstat = WaitToCompleteNoProgress($removeDeployment.operationId)
+	$removeDeployment = Remove-AzureDeployment -Slot $slot -ServiceName $serviceName -force 
 	
-	write-progress -id 2 -activity "Deleting Deployment" -Status $opstat
-	Write-Output "$(Get-Date –f $timeStampFormat) - Deleting Deployment: $opstat"
-	
-	sleep -Seconds 10
+	write-progress -id 2 -activity "Deleting Deployment: Complete" -completed -Status $removeDeployment
+	Write-Output "$(Get-Date –f $timeStampFormat) - Deleting Deployment: Complete"
 }
 
-function WaitToCompleteNoProgress($operationId)
-{
-	$result = Get-OperationStatus -OperationId $operationId
-	
-	while ([string]::Equals($result, "InProgress"))
-	{
-		sleep -Seconds 1
-		$result = Get-OperationStatus -OperationId $operationId
-	}
-	
-	return $result
-}
 
 function Publish()
 {
-	$deployment = Get-Deployment -ServiceName $serviceName -Slot $slot -ErrorVariable a -ErrorAction silentlycontinue 
+	$deployment = Get-AzureRole -ServiceName $serviceName -Slot $slot -ErrorVariable a -ErrorAction silentlycontinue 
     if ($a[0] -ne $null)
     {
-        write-host "No deployment is detected. Creating a new deployment. "
+        Write-Output "$(Get-Date –f $timeStampFormat) - No deployment is detected. Creating a new deployment. "
     }
-    #check for existing deployment and then either upgrade, delete + deploy, or cancel according to $alwaysDeleteExistingDeployments and $enableDeploymentUpgrade boolean variables
-	if ($deployment.Name -ne $null)
+    
+	#check for existing deployment and then either upgrade, delete + deploy, or cancel according to $alwaysDeleteExistingDeployments and $enableDeploymentUpgrade boolean variables
+	if ($deployment.RoleName -ne $null)
 	{
 		switch ($alwaysDeleteExistingDeployments)
 	    {
@@ -138,111 +104,115 @@ function CreateNewDeployment()
 	write-progress -id 3 -activity "Creating New Deployment" -Status "In progress"
 	Write-Output "$(Get-Date –f $timeStampFormat) - Creating New Deployment: In progress"
 
-	$newdeployment = New-Deployment -Slot $slot -Package $packageLocation -Configuration $cloudConfigLocation -label $deploymentLabel -ServiceName $serviceName -StorageAccountName $storageAccountName
-	$opstat = WaitToCompleteNoProgress($newdeployment.operationId)
-	
-	write-progress -id 3 -activity "Creating New Deployment" -Status $opstat
-    
-    $completeDeployment = Get-Deployment -ServiceName $serviceName -Slot $slot
+	$newdeployment = New-AzureDeployment -Slot $slot -Package $packageLocation -Configuration $cloudConfigLocation -label $deploymentLabel -ServiceName $serviceName 
+
+	$completeDeployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
     $completeDeploymentID = $completeDeployment.deploymentid
-	Write-Output "$(Get-Date –f $timeStampFormat) - Creating New Deployment: $opstat, Deployment ID: $completeDeploymentID"
-    
+
+    write-progress -id 3 -activity "Creating New Deployment" -completed -Status "Complete"
+    Write-Output "$(Get-Date –f $timeStampFormat) - Creating New Deployment: Complete, Deployment ID: $completeDeploymentID"
+
 	StartInstances
+    
 }
 
 function UpgradeDeployment()
 {
-	write-progress -id 3 -activity "Upgrading Deployment" -Status "In progress"
-	Write-Output "$(Get-Date –f $timeStampFormat) - Upgrading Deployment: In progress"
+    write-progress -id 3 -activity "Upgrading Deployment" -Status "In progress"
+    Write-Output "$(Get-Date –f $timeStampFormat) - Upgrading Deployment: In progress"
 
-	Update-Deployment -Slot $slot -Package $packageLocation -Configuration $cloudConfigLocation -label $deploymentLabel -ServiceName $serviceName -StorageAccountName $storageAccountName | 
-   Get-OperationStatus -WaitToComplete
-    
-    #$opstat = WaitToCompleteNoProgress($newdeployment.operationId)
-	
-	#write-progress -id 3 -activity "Upgrading New Deployment" -Status $opstat
-    
-    $completeDeployment = Get-Deployment -ServiceName $serviceName -Slot $slot
+    # perform Update-Deployment
+    $setdeployment = Set-AzureDeployment -Upgrade -Slot $slot -Package $packageLocation -Configuration $cloudConfigLocation -label $deploymentLabel -ServiceName $serviceName -Force
+
+    $completeDeployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
     $completeDeploymentID = $completeDeployment.deploymentid
-	Write-Output "$(Get-Date –f $timeStampFormat) - Upgrading Deployment: Succeeded, Deployment ID: $completeDeploymentID"
 
+    write-progress -id 3 -activity "Upgrading Deployment" -completed -Status "Complete"
+    Write-Output "$(Get-Date –f $timeStampFormat) - Upgrading Deployment: Complete, Deployment ID: $completeDeploymentID"
 }
 
 function StartInstances()
 {
-	write-progress -id 4 -activity "Starting Instances" -status "In progress"
-	Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instances: In progress"
+    write-progress -id 4 -activity "Starting Instances" -status "In progress"
+    Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instances: In progress"
 
-	$run = Set-DeploymentStatus -Slot $slot -ServiceName $serviceName -Status Running
-	$deployment = Get-Deployment -ServiceName $serviceName -Slot $slot
-	$oldStatusStr = @("") * $deployment.RoleInstanceList.Count
-	
-	while (-not(AllInstancesRunning($deployment.RoleInstanceList)))
-	{
-		$i = 1
-		foreach ($roleInstance in $deployment.RoleInstanceList)
-		{
-			$instanceName = $roleInstance.InstanceName
-			$instanceStatus = $roleInstance.InstanceStatus
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $runstatus = $deployment.Status
 
-			if ($oldStatusStr[$i - 1] -ne $roleInstance.InstanceStatus)
-			{
-				$oldStatusStr[$i - 1] = $roleInstance.InstanceStatus
-				Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instance '$instanceName': $instanceStatus"
-			}
+    if ($runstatus -ne 'Running') 
+    {
+        $run = Set-AzureDeployment -Slot $slot -ServiceName $serviceName -Status Running
+    }
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $oldStatusStr = @("") * $deployment.RoleInstanceList.Count
 
-			write-progress -id (4 + $i) -activity "Starting Instance '$instanceName'" -status "$instanceStatus"
-			$i = $i + 1
-		}
+    while (-not(AllInstancesRunning($deployment.RoleInstanceList)))
+    {
+        $i = 1
+        foreach ($roleInstance in $deployment.RoleInstanceList)
+        {
+            $instanceName = $roleInstance.InstanceName
+            $instanceStatus = $roleInstance.InstanceStatus
 
-		sleep -Seconds 1
+            if ($oldStatusStr[$i - 1] -ne $roleInstance.InstanceStatus)
+            {
+                $oldStatusStr[$i - 1] = $roleInstance.InstanceStatus
+                Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instance '$instanceName': $instanceStatus"
+            }
 
-		$deployment = Get-Deployment -ServiceName $serviceName -Slot $slot
-	}
+            write-progress -id (4 + $i) -activity "Starting Instance '$instanceName'" -status "$instanceStatus"
+            $i = $i + 1
+        }
 
-	$i = 1
-	foreach ($roleInstance in $deployment.RoleInstanceList)
-	{
-		$instanceName = $roleInstance.InstanceName
-		$instanceStatus = $roleInstance.InstanceStatus
+        sleep -Seconds 1
 
-		if ($oldStatusStr[$i - 1] -ne $roleInstance.InstanceStatus)
-		{
-			$oldStatusStr[$i - 1] = $roleInstance.InstanceStatus
-			Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instance '$instanceName': $instanceStatus"
-		}
+        $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    }
 
-		write-progress -id (4 + $i) -activity "Starting Instance '$instanceName'" -status "$instanceStatus"
-		$i = $i + 1
-	}
-	
-	$opstat = Get-OperationStatus -operationid $run.operationId
-	
-	write-progress -id 4 -activity "Starting Instances" -status $opstat
-	Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instances: $opstat"
+    $i = 1
+    foreach ($roleInstance in $deployment.RoleInstanceList)
+    {
+        $instanceName = $roleInstance.InstanceName
+        $instanceStatus = $roleInstance.InstanceStatus
+
+        if ($oldStatusStr[$i - 1] -ne $roleInstance.InstanceStatus)
+        {
+            $oldStatusStr[$i - 1] = $roleInstance.InstanceStatus
+            Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instance '$instanceName': $instanceStatus"
+        }
+
+        $i = $i + 1
+    }
+
+    $deployment = Get-AzureDeployment -ServiceName $serviceName -Slot $slot
+    $opstat = $deployment.Status 
+
+    write-progress -id 4 -activity "Starting Instances" -completed -status $opstat
+    Write-Output "$(Get-Date –f $timeStampFormat) - Starting Instances: $opstat"
 }
 
 function AllInstancesRunning($roleInstanceList)
 {
-	foreach ($roleInstance in $roleInstanceList)
-	{
-		if ($roleInstance.InstanceStatus -ne "Ready")
-		{
-			return $false
-		}
-	}
-	
-	return $true
+    foreach ($roleInstance in $roleInstanceList)
+    {
+        if ($roleInstance.InstanceStatus -ne "ReadyRole")
+        {
+            return $false
+        }
+    }
+
+    return $true
 }
 
 
+
 Write-Output "$(Get-Date –f $timeStampFormat) - Azure Cloud App deploy script started."
-Write-Output "$(Get-Date –f $timeStampFormat) - Preparing deployment of $deploymentLabel for $subscriptionname with Subscription ID $subscriptionid."
+Write-Output "$(Get-Date –f $timeStampFormat) - Preparing deployment of $deploymentLabel into Service: $servicename Environment: $slot"
 
 Publish
 
-$deployment = Get-Deployment -slot $slot -serviceName $servicename
+$deployment = Get-AzureDeployment -slot $slot -serviceName $servicename
 $deploymentUrl = $deployment.Url
 
-Write-Output "$(Get-Date –f $timeStampFormat) - Created Cloud App with URL $deploymentUrl."
-Write-Output "$(Get-Date –f $timeStampFormat) - Azure Cloud App deploy script finished."
+Write-Output "$(Get-Date –f $timeStampFormat) - Created Cloud Service with URL $deploymentUrl."
+Write-Output "$(Get-Date –f $timeStampFormat) - Azure Cloud Service deploy script finished."
